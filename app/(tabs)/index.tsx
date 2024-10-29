6b88235a5378bb9e583ac0558ee0e29126cfcbef
FelipeGuedesGoncalves <felipeguedes6559@gmail.com>
2024-10-29 12:10:06 -0300

    criação de usuário no firebase

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,48 @@
-import { StyleSheet, View } from 'react-native';
-import { auth } from '@/components/Firebase'
+import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { auth } from '@/components/Firebase';
+import { useState, useEffect } from 'react'
+import React from 'react';
+
 
 export default function HomeScreen() {
-  auth
-    .createUserWithEmailAndPassword('email@email.com', '12345678')
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState({})
+
+useEffect(() => {
+  if (user) {
+    console.log('Navegar para a home')
+  }
+})
+
+  async function createUser() {
+    auth
+    .createUserWithEmailAndPassword(email, password)
     .then((response) => {
-      console.log
+      setUser(response.user)
     })
+    .catch((error)=> {
+      console.log(error)
+    })
+  }
+
   return (
-    <View>
-      
+    <View style={{margin: 24}}>
+      <TextInput
+        placeholder='Email'
+        onChangeText={(text) => setEmail(text)}
+      />
+      <TextInput
+        placeholder='Senha'
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry = {true} 
+      />
+      <Button title='Criar Usuário'
+      onPress={() => createUser()}
+      />
+
+
     </View>
   );
 }
