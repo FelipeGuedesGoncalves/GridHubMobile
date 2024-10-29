21b6d48e49441b67715d445a1574ab8d34dbb9e6
FelipeGuedesGoncalves <felipeguedes6559@gmail.com>
2024-10-29 11:33:51 -0300

    erros 'released' e 'default has been created' foram resolvidos e app abre agora

--- components/Firebase.tsx
+++ components/Firebase.tsx
@@ -7,7 +7,15 @@ const firebaseConfig = {
     apiKey:'AIzaSyB2DjSl-1nDQDMG5OT-H7L3ua2IYuyxFTA',
     databaseURL:'https://insightwisemobile-default-rtdb.firebaseio.com/',
     storageBucket:'gs://insightwisemobile.appspot.com',
+    messagingSenderId: '', 
     projectId:'insightwisemobile'
 }
 
 firebase.initializeApp(firebaseConfig)
+
+const apps = firebase.apps
+
+apps.forEach(app => console.log(app.name))
+
+const auth = firebase.app().auth()
+export {firebase, auth}
