ad1a8be9b9e9fdd79e279c42260a0ea1e4fe5f05
FelipeGuedesGoncalves <felipeguedes6559@gmail.com>
2024-11-02 16:53:03 -0300

    Gráficos funcionando com firebase

--- routes/Dashboard.tsx
+++ routes/Dashboard.tsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
 import { auth, database } from '@/components/Firebase';
 import { dadosIniciaisAnalise } from '@/models/dadosIniciaisAnalise';
+import { BarChart, PieChart, ProgressChart } from 'react-native-chart-kit';
+
+const screenWidth = Dimensions.get('window').width;
 
 export default function Dashboard() {
     const [analysisData, setAnalysisData] = useState(dadosIniciaisAnalise); // Usa a estrutura inicial
@@ -11,7 +14,7 @@ export default function Dashboard() {
             const user = auth.currentUser;
             if (user) {
                 const analysisRef = database.ref(`analises/${user.uid}`);
-                analysisRef.once('value', (snapshot) => {
+                analysisRef.on('value', (snapshot) => {
                     if (snapshot.exists()) {
                         setAnalysisData(snapshot.val());
                     } else {
@@ -23,26 +26,149 @@ export default function Dashboard() {
         fetchAnalysisData();
     }, []);
 
+    // Preparando os dados para os gráficos
+    const atividadesConcluidas = parseInt(analysisData.hoje.atividadesConcluidas) || 0;
+    const atividadesInacabadas = parseInt(analysisData.hoje.atividadesInacabadas) || 0;
+    
+    const funcoesMaisUtilizadasColors = [
+        '#FF6384', // Cor para a primeira função
+        '#36A2EB', // Cor para a segunda função
+        '#FFCE56', // Cor para a terceira função
+        '#4BC0C0', // Cor para a quarta função
+        '#9966FF', // Cor para a quinta função
+        '#FF9F40', // Cor para a sexta função
+    ];
+    
+    const funcoesMaisUtilizadasData = Object.entries(analysisData.hoje.funcoesMaisUtilizadas).map(([funcao, valor], index) => ({
+        name: funcao,
+        population: parseInt(valor) || 0,
+        color: funcoesMaisUtilizadasColors[index % funcoesMaisUtilizadasColors.length], // Atribui cor baseado no índice
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15,
+    }));
+    
+
+    const utilizacaoDeFuncoes = [
+        {
+            name: "Utilizadas",
+            population: parseInt(analysisData.hoje.utilizacaoDeFuncoes.funcoesUtilizadas) || 0,
+            color: "#00FF00", // Verde
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 15
+        },
+        {
+            name: "Não Utilizadas",
+            population: parseInt(analysisData.hoje.utilizacaoDeFuncoes.funcoesNaoUtilizadas) || 0,
+            color: "#FF0000", // Vermelho
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 15
+        }
+    ];
+
+    const tempoInatividade = parseInt(analysisData.hoje.tempoInatividade) || 0;
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Dashboard</Text>
             <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
                 {analysisData ? (
                     <>
-                        <Text style={styles.sectionTitle}>Análise de Hoje</Text>
-                        <Text>Atividades Concluídas: {analysisData.hoje.atividadesConcluidas}</Text>
-                        <Text>Atividades Inacabadas: {analysisData.hoje.atividadesInacabadas}</Text>
-                        <Text>Tempo de Inatividade: {analysisData.hoje.tempoInatividade} minutos</Text>
-                        
-                        <Text style={styles.sectionTitle}>Funções Mais Utilizadas Hoje</Text>
-                        {Object.entries(analysisData.hoje.funcoesMaisUtilizadas).map(([funcao, valor]) => (
-                            <Text key={funcao}>{funcao}: {valor}</Text>
-                        ))}
 
-                        <Text style={styles.sectionTitle}>Utilização de Funções Hoje</Text>
-                        <Text>Funções Utilizadas: {analysisData.hoje.utilizacaoDeFuncoes.funcoesUtilizadas}</Text>
-                        <Text>Funções Não Utilizadas: {analysisData.hoje.utilizacaoDeFuncoes.funcoesNaoUtilizadas}</Text>
                         
+                        <Text style={styles.sectionTitle}>Atividades Concluídas e Inacabadas</Text>
+                        <BarChart
+                            data={{
+                                labels: ["Concluídas", "Inacabadas"],
+                                datasets: [
+                                    {
+                                        data: [atividadesConcluidas, atividadesInacabadas]
+                                    }
+                                ]
+                            }}
+                            yAxisLabel=''
+                            yAxisSuffix=''
+                            width={screenWidth}
+                            height={220}
+                            chartConfig={{
+                                backgroundColor: '#fff',
+                                backgroundGradientFrom: '#fff',
+                                backgroundGradientTo: '#fff',
+                                decimalPlaces: 0,
+                                color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`,
+                                style: {
+                                    borderRadius: 16,
+                                },
+                            }}
+                            style={{
+                                marginVertical: 8,
+                                borderRadius: 16,
+                            }}
+                        />
+
+                        <Text style={styles.sectionTitle}>Funções Mais Utilizadas</Text>
+                        <PieChart
+                            data={funcoesMaisUtilizadasData}
+                            width={screenWidth}
+                            height={220}
+                            chartConfig={{
+                                backgroundColor: '#fff',
+                                backgroundGradientFrom: '#fff',
+                                backgroundGradientTo: '#fff',
+                                color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`,
+                                style: {
+                                    borderRadius: 16,
+                                },
+                            }}
+                            accessor={"population"}
+                            backgroundColor={"transparent"}
+                            paddingLeft={"15"}
+                            center={[10, 50]}
+                            absolute
+                        />
+
+                        <Text style={styles.sectionTitle}>Tempo de Inatividade</Text>
+                        <ProgressChart
+                            data={{
+                                labels: ["Inatividade"], // opcional
+                                data: [tempoInatividade / 60] // Normaliza para um valor entre 0 e 1
+                            }}
+                            width={screenWidth}
+                            height={220}
+                            strokeWidth={16}
+                            radius={32}
+                            chartConfig={{
+                                backgroundColor: '#fff',
+                                backgroundGradientFrom: '#fff',
+                                backgroundGradientTo: '#fff',
+                                color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`,
+                                style: {
+                                    borderRadius: 16,
+                                },
+                            }}
+                            hideLegend={false}
+                        />
+
+                        <Text style={styles.sectionTitle}>Funções Utilizadas</Text>
+                        <PieChart
+                            data={utilizacaoDeFuncoes}
+                            width={screenWidth}
+                            height={220}
+                            chartConfig={{
+                                backgroundColor: '#fff',
+                                backgroundGradientFrom: '#fff',
+                                backgroundGradientTo: '#fff',
+                                color: (opacity = 1) => `rgba(0, 0, 255, ${opacity})`,
+                                style: {
+                                    borderRadius: 16,
+                                },
+                            }}
+                            accessor={"population"}
+                            backgroundColor={"transparent"}
+                            paddingLeft={"15"}
+                            center={[10, 50]}
+                            absolute
+                        />
+
                     </>
                 ) : (
                     <Text>Carregando dados de análise...</Text>
