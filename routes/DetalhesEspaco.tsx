4131bd6cfd797e33c4599b52971468513144d7c5
FelipeGuedesGoncalves <felipeguedes6559@gmail.com>
2024-11-22 11:18:42 -0300

    Espacos alugaveis e Detalhes do Espaco primeiro commit

---
+++ routes/DetalhesEspaco.tsx
@@ -0,0 +1,111 @@
+import { appcolors } from '@/styles/appcolors';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { firebase } from '@/components/Firebase'; // Para acessar o Firebase
+import { ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'; // Importando AsyncStorage
+
+export default function DetalhesEspaco() {
+  const [espaco, setespaco] = useState<any>(null);
+  const [userData, setUserData] = useState<any>(null);
+  const [locationData, setLocationData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const espacoId = await AsyncStorage.getItem('espacoSendoExibido'); // Recuperando o espacoId do AsyncStorage
+
+      if (espacoId) {
+        // Buscar os dados da espaco pelo ID
+        const espacoRef = firebase.database().ref('espacos').child(espacoId);
+        espacoRef.once('value', (snapshot) => {
+          const espacoData = snapshot.val();
+          setespaco(espacoData);
+
+          // Quando pegar o userId da espaco, buscar as informações do locatário
+          if (espacoData && espacoData.userId) {
+            const userRef = firebase.database().ref('usuario').child(espacoData.userId);
+            userRef.once('value', (userSnapshot) => {
+              setUserData(userSnapshot.val());  // Garantir que estamos pegando os dados do locatário
+            });
+          }
+
+          // Quando pegar o espacoId da espaco, buscar as informações de localização
+          if (espacoData && espacoData.espacoId) {
+            const espacoRef = firebase.database().ref('espacos').child(espacoData.espacoId);
+            espacoRef.once('value', (espacoSnapshot) => {
+              setLocationData(espacoSnapshot.val());
+            });
+          }
+        });
+      }
+    };
+
+    fetchData();
+  }, []); // Executa apenas uma vez quando o componente é montado
+
+  return (
+    <ScrollView>
+        <View style={styles.container}>
+            <Text style={styles.title}>{espaco?.nomeEspaco || 'N/A'}</Text>
+
+            <View style={styles.details}>
+                <View style={styles.infoblock}>
+                    <Text style={styles.detailsSubtitle}>Especificidades</Text>
+                    <Text style={styles.detailsContent}>Área total: {espaco?.areaTotal || 'N/A'}m²</Text>
+                    <Text style={styles.detailsContent}>Orientação Solar: {espaco?.orientacaoSolar || 'N/A'}</Text>
+                    <Text style={styles.detailsContent}>Média Anual de Radiação Solar: {espaco?.mediaRadiacao || 'N/A'}kWh/m²</Text>
+                    <Text style={styles.detailsContent}>Topografia: {espaco?.topografia || 'N/A'}</Text>
+                    <Text style={styles.detailsContent}>Velocidade média do vento da região: {espaco?.velocidadeVento || 'N/A'}m/s </Text>
+                    <Text style={styles.detailsContent}>Endereço: {espaco?.endereco || 'N/A'} m²</Text>
+                    <Text style={styles.detailsContent}>Suporte para as energias: {espaco?.fontesEnergia || 'N/A'}</Text>
+                </View>
+                <View style={styles.infoblock}>
+                    <Text style={styles.detailsSubtitle}>Informações do Locador</Text>
+                    <Text style={styles.detailsContent}>Email: {userData?.email || 'Não disponível'}</Text>
+                    <Text style={styles.detailsContent}>Nome: {userData?.name || 'Não disponível'}</Text>
+                    <Text style={styles.detailsContent}>Telefone: {userData?.telefone || 'Não disponível'}</Text>
+                </View>
+            </View>
+        </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+        alignItems: 'center',
+        paddingTop: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        padding: 16,
+        color: appcolors.azulescuro,
+      },
+    details: {
+        width: '80%',
+        marginTop: 10,
+        marginBottom: 40,
+    },
+    infoblock: {},
+    detailsTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: appcolors.azulescuro,
+        textAlign: 'center',
+    },
+    detailsSubtitle: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        color: appcolors.azulescuro,
+        marginTop: 20,
+    },
+    detailsContent: {
+        fontSize: 14,
+        color: appcolors.azulescuro,
+        marginVertical: 5,
+    },
+});
+
--- routes/EspacosAlugaveis.tsx
+++ routes/EspacosAlugaveis.tsx
@@ -1,26 +1,155 @@
-import { appcolors } from '@/styles/appcolors';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useFocusEffect } from '@react-navigation/native';
+import { appcolors } from '@/styles/appcolors';
+import { firebase } from '@/components/Firebase';
+import { globalstyles } from '@/styles/globalstyles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import DetalhesEspaco from './DetalhesEspaco';
 
+const Stack = createStackNavigator();
+
+function EspacoScreen({ navigation }: { navigation: any }) {
+  const [espacos, setEspacos] = React.useState([]);
+
+  const fetchEspacos = React.useCallback(() => {
+    const espacoRef = firebase.database().ref('espacos');
+
+    espacoRef.once('value', (snapshot) => {
+      const data = snapshot.val() || {};
+      const uniqueEspacos = Object.keys(data).map((key) => ({
+        id: key,
+        ...data[key],
+      }));
+      setEspacos(uniqueEspacos);
+    });
+
+    // Garantindo que nenhum listener fique ativo
+    return () => espacoRef.off();
+  }, []);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      // Atualiza os espaços toda vez que a tela ganha foco
+      fetchEspacos();
+    }, [fetchEspacos])
+  );
+
+  const handleVerMais = async (espacoId: string) => {
+    // Armazenar o espacoId no AsyncStorage
+    await AsyncStorage.setItem('espacoSendoExibido', espacoId);
+    // Redirecionar para a tela de detalhes
+    navigation.navigate('DetalhesEspaco');
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.card}>
+      <Text style={styles.cardTitle}>{item.nomeEspaco}</Text>
+      <Text style={styles.cardSubtitle}>Endereço:</Text>
+      <Text style={styles.cardContent}>{item.endereco}</Text>
+      <Text style={styles.cardSubtitle}>Área Total:</Text>
+      <Text style={styles.cardContent}>{item.areaTotal} m²</Text>
+      <TouchableOpacity
+        style={globalstyles.morebutton}
+        onPress={() => handleVerMais(item.id)}
+      >
+        <Text style={globalstyles.morebuttontext}>Ver Mais</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Espaços Disponíveis</Text>
+      <Text style={styles.subtitle}>
+        Encontre espaços ideais para geração de energia renovável. Explore e veja detalhes dos
+        espaços cadastrados.
+      </Text>
+      <FlatList
+        data={espacos}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={styles.flatList}
+      />
+    </View>
+  );
+}
 
 export default function EspacosAlugaveis() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Espacos Alugáveis</Text>
-        </View>
-    );
+  return (
+    <Stack.Navigator initialRouteName="EspacoScreen">
+      <Stack.Screen
+        name="EspacoScreen"
+        component={EspacoScreen}
+        options={{
+          title: 'Espaços',
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="DetalhesEspaco"
+        component={DetalhesEspaco}
+        options={{
+          title: 'Detalhes do Espaço',
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFFFFF',
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        padding: 16,
-        color: appcolors.azulescuro
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    paddingTop: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    padding: 16,
+    color: appcolors.azulescuro,
+  },
+  subtitle: {
+    fontSize: 11,
+    paddingHorizontal: 30,
+    color: appcolors.azulescuro,
+    justifyContent: 'center',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  card: {
+    backgroundColor: appcolors.branco,
+    width: '90%',
+    elevation: 20,
+    shadowRadius: 100,
+    shadowOffset: { height: 10, width: 20 },
+    shadowOpacity: 10,
+    borderRadius: 30,
+    padding: 20,
+    margin: 20,
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: appcolors.azulescuro,
+  },
+  cardSubtitle: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: appcolors.azulescuro,
+    marginTop: 20,
+  },
+  cardContent: {
+    fontSize: 14,
+    color: appcolors.azulescuro,
+  },
+  flatList: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 80,
+  },
 });
